99b4e7d795f8ef8acaa363c19d003fe8bb0db4a9
Swetha Ganapathy subramanian <swethaganapathy2@gmail.com>
2020-09-15 20:41:16 -0700

    Few Changes in library

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _Button.default;
+  }
+});
+Object.defineProperty(exports, "Input", {
+  enumerable: true,
+  get: function get() {
+    return _Input.default;
+  }
+});
+
+var _Button = _interopRequireDefault(require("./Button"));
+
+var _Input = _interopRequireDefault(require("./Input"));
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Button from "./Button";
 import Input from "./Input";
 
-export { Button, Input };
+export { Button };
+export { Input };
